227c8e6cb274613404ad2740c1596763a67dad57
eni <129673786+enitama@users.noreply.github.com>
2023-04-22 02:26:42 +0900

    Set up pre-commit with clang-format
    Arbitrarily chose chromium since it's related to the web, no strong reason. Open to change if there's a good reason

--- memvfs/main.c
+++ memvfs/main.c
@@ -6,67 +6,62 @@
 
 // TODO: clean this up
 
-static const char dbbytes[] = {0/*redacted*/};
+static const char dbbytes[] = {0 /*redacted*/};
 
-int sqlite3_memvfs_init(
-  sqlite3 *db, 
-  char **pzErrMsg, 
-  const sqlite3_api_routines *pApi
-);
-int core_init(const char* dummy)
-{
-    int nErr = 0;
-    nErr += sqlite3_auto_extension((void *)sqlite3_memvfs_init);
-    return nErr ? SQLITE_ERROR : SQLITE_OK;
+int sqlite3_memvfs_init(sqlite3* db,
+                        char** pzErrMsg,
+                        const sqlite3_api_routines* pApi);
+int core_init(const char* dummy) {
+  int nErr = 0;
+  nErr += sqlite3_auto_extension((void*)sqlite3_memvfs_init);
+  return nErr ? SQLITE_ERROR : SQLITE_OK;
 }
 
-int32_t count_vowels()
-{
+int32_t count_vowels() {
   uint64_t length = extism_input_length();
-  uint8_t *input = malloc(length);
+  uint8_t* input = malloc(length);
   extism_load_input(input, length);
 
   char out[128];
   int rc;
 
-  
-  char *memuri = sqlite3_mprintf("file:whatever?ptr=0x%p&sz=%d&freeonclose=1",
+  char* memuri = sqlite3_mprintf("file:whatever?ptr=0x%p&sz=%d&freeonclose=1",
                                  &dbbytes, sizeof(dbbytes));
 
-  sqlite3 *db;
+  sqlite3* db;
   sqlite3_db_config(db, SQLITE_DBCONFIG_ENABLE_LOAD_EXTENSION, 1, NULL);
-	if (sqlite3_memvfs_init(db, NULL, NULL) != SQLITE_OK_LOAD_PERMANENTLY)
-	{
+  if (sqlite3_memvfs_init(db, NULL, NULL) != SQLITE_OK_LOAD_PERMANENTLY) {
     int n = snprintf(out, 128, "error loading memvfs");
     uint64_t offs_ = extism_alloc(n);
-    extism_store(offs_, (const uint8_t *)out, n);
-    extism_output_set(offs_, n);
-    return 0;
-	}
-
-  rc = sqlite3_open_v2(memuri, &db, SQLITE_OPEN_READONLY | SQLITE_OPEN_URI, "memvfs");
-  if (rc != SQLITE_OK) {
-    int n = snprintf(out, 128, "open memvfs: %s\n", sqlite3_errmsg(db));
-    uint64_t offs_ = extism_alloc(n);
-    extism_store(offs_, (const uint8_t *)out, n);
+    extism_store(offs_, (const uint8_t*)out, n);
     extism_output_set(offs_, n);
     return 0;
   }
-  //sqlite3_free(memuri);
 
-  sqlite3_stmt *stmt;
+  rc = sqlite3_open_v2(memuri, &db, SQLITE_OPEN_READONLY | SQLITE_OPEN_URI,
+                       "memvfs");
+  if (rc != SQLITE_OK) {
+    int n = snprintf(out, 128, "open memvfs: %s\n", sqlite3_errmsg(db));
+    uint64_t offs_ = extism_alloc(n);
+    extism_store(offs_, (const uint8_t*)out, n);
+    extism_output_set(offs_, n);
+    return 0;
+  }
+  // sqlite3_free(memuri);
+
+  sqlite3_stmt* stmt;
   if (sqlite3_prepare_v2(db, "SELECT * FROM sqlite_schema", -1, &stmt, NULL) !=
       SQLITE_OK) {
     int n = snprintf(out, 128, "prepare: %s\n", sqlite3_errmsg(db));
     uint64_t offs_ = extism_alloc(n);
-    extism_store(offs_, (const uint8_t *)out, n);
+    extism_store(offs_, (const uint8_t*)out, n);
     extism_output_set(offs_, n);
 
     return 0;
   }
 
-  //int n = snprintf(out, 128, "{\"count\": %lld}", count);
-  //int n = snprintf(out, 128, "input: %s", input);
+  // int n = snprintf(out, 128, "{\"count\": %lld}", count);
+  // int n = snprintf(out, 128, "input: %s", input);
 
   int n;
   n = snprintf(out, 128, "input: %s, uri=%s", input, memuri);
@@ -75,7 +70,7 @@ int32_t count_vowels()
   }
 
   uint64_t offs_ = extism_alloc(n);
-  extism_store(offs_, (const uint8_t *)out, n);
+  extism_store(offs_, (const uint8_t*)out, n);
   extism_output_set(offs_, n);
 
   return 0;
--- unixvfs/main.c
+++ unixvfs/main.c
@@ -9,14 +9,16 @@
 
 // TODO: clean this up
 
-static char *out_start;
-static char *out_head;
+static char* out_start;
+static char* out_head;
 
-static void errorLogCallback(void *pStatement, int iErrCode, const char *zMsg) {
-  out_head += snprintf(out_head, 1024-(out_head-out_start), "statement: %s\n(%d) %s\n",*(char**)pStatement, iErrCode, zMsg);
+static void errorLogCallback(void* pStatement, int iErrCode, const char* zMsg) {
+  out_head +=
+      snprintf(out_head, 1024 - (out_head - out_start),
+               "statement: %s\n(%d) %s\n", *(char**)pStatement, iErrCode, zMsg);
 }
 
-int lstat_ignore_enotcapable(const char *path, struct stat *buf) {
+int lstat_ignore_enotcapable(const char* path, struct stat* buf) {
   int err = lstat(path, buf);
   if (err && errno == ENOTCAPABLE) {
     *buf = (struct stat){0};
@@ -25,22 +27,18 @@ int lstat_ignore_enotcapable(const char *path, struct stat *buf) {
   return err;
 }
 
-int sqlite3_memvfs_init(
-  sqlite3 *db, 
-  char **pzErrMsg, 
-  const sqlite3_api_routines *pApi
-);
-int core_init(const char* dummy)
-{
-    int nErr = 0;
-    nErr += sqlite3_auto_extension((void *)sqlite3_memvfs_init);
-    return nErr ? SQLITE_ERROR : SQLITE_OK;
+int sqlite3_memvfs_init(sqlite3* db,
+                        char** pzErrMsg,
+                        const sqlite3_api_routines* pApi);
+int core_init(const char* dummy) {
+  int nErr = 0;
+  nErr += sqlite3_auto_extension((void*)sqlite3_memvfs_init);
+  return nErr ? SQLITE_ERROR : SQLITE_OK;
 }
 
-int32_t count_vowels()
-{
+int32_t count_vowels() {
   uint64_t length = extism_input_length();
-  uint8_t *input = malloc(length);
+  uint8_t* input = malloc(length);
   extism_load_input(input, length);
 
   char out[1024];
@@ -50,8 +48,8 @@ int32_t count_vowels()
   out_head = out;
   sqlite3_config(SQLITE_CONFIG_LOG, errorLogCallback, NULL);
 
-  sqlite3_vfs *default_vfs = sqlite3_vfs_find(NULL);
-  default_vfs->xSetSystemCall(NULL, "lstat", (void *)lstat_ignore_enotcapable);
+  sqlite3_vfs* default_vfs = sqlite3_vfs_find(NULL);
+  default_vfs->xSetSystemCall(NULL, "lstat", (void*)lstat_ignore_enotcapable);
 
   // struct stat st;
   // stat("/path/to/your.db", &st);
@@ -64,24 +62,25 @@ int32_t count_vowels()
   //   return 0;
   // }
 
-  sqlite3 *db;
+  sqlite3* db;
 
   rc = sqlite3_open("/path/to/your.db", &db);
   if (rc != SQLITE_OK) {
-    //int n = snprintf(out, 128, "open failed: %s (log size: %d)\n", sqlite3_errmsg(db), out_head-out_start);
-    int n = out_head-out_start;
+    // int n = snprintf(out, 128, "open failed: %s (log size: %d)\n",
+    // sqlite3_errmsg(db), out_head-out_start);
+    int n = out_head - out_start;
     uint64_t offs_ = extism_alloc(n);
-    extism_store(offs_, (const uint8_t *)out, n);
+    extism_store(offs_, (const uint8_t*)out, n);
     extism_output_set(offs_, n);
     return 0;
   }
 
-  sqlite3_stmt *stmt;
-  if (sqlite3_prepare_v2(db, "SELECT name FROM sqlite_schema", -1, &stmt, NULL) !=
-      SQLITE_OK) {
+  sqlite3_stmt* stmt;
+  if (sqlite3_prepare_v2(db, "SELECT name FROM sqlite_schema", -1, &stmt,
+                         NULL) != SQLITE_OK) {
     int n = snprintf(out, 128, "prepare: %s\n", sqlite3_errmsg(db));
     uint64_t offs_ = extism_alloc(n);
-    extism_store(offs_, (const uint8_t *)out, n);
+    extism_store(offs_, (const uint8_t*)out, n);
     extism_output_set(offs_, n);
 
     return 0;
@@ -94,10 +93,12 @@ int32_t count_vowels()
   }
 
   uint64_t offs_ = extism_alloc(n);
-  extism_store(offs_, (const uint8_t *)out, n);
+  extism_store(offs_, (const uint8_t*)out, n);
   extism_output_set(offs_, n);
 
   return 0;
 }
 
-int main() { count_vowels(); }
+int main() {
+  count_vowels();
+}
